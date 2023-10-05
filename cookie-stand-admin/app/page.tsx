f1e316c8119c325451595f84a36b66dd35154ab7
ammarAltarawneh <tarawnhammar@gmail.com>
2023-10-05 22:19:43 +0000

    update

--- cookie-stand-admin/app/page.tsx
+++ cookie-stand-admin/app/page.tsx
@@ -1,8 +1,36 @@
+"use client"
+import React, { useState } from 'react';
+
 export default function Home() {
+  const [cookieStandData, setCookieStandData] = useState<{
+    Location: string;
+    'Minimum Customers Per Hour': number;
+    'Maximum Customers Per Hour': number;
+    'Average Cookies Per Sale': number;
+  } | null>(null);
+
   function CookieStandForm() {
+    const [location, setLocation] = useState('');
+    const [minCustomers, setMinCustomers] = useState<number | ''>('');
+    const [maxCustomers, setMaxCustomers] = useState<number | ''>('');
+    const [avgCookies, setAvgCookies] = useState<number | ''>('');
+
+    const handleCreateClick = () => {
+      if (location && !isNaN(parseFloat(minCustomers as string)) && !isNaN(parseFloat(maxCustomers as string)) && !isNaN(parseFloat(avgCookies as string))) {
+        const newCookieStandData = {
+          Location: location,
+          'Minimum Customers Per Hour': parseFloat(minCustomers as string),
+          'Maximum Customers Per Hour': parseFloat(maxCustomers as string),
+          'Average Cookies Per Sale': parseFloat(avgCookies as string),
+        };
+
+        setCookieStandData(newCookieStandData);
+      }
+    };
+
     return (
       <div className="flex items-center justify-center h-screen">
-        <form className="bg-green-200 p-7 rounded-lg shadow-md w-90">
+        <form className="bg-green-200 p-8 rounded-lg shadow-md w-90">
           <h1 className="text-2xl font-semibold mb-4">Create Cookie Stand</h1>
 
           <div className="mb-4">
@@ -11,6 +39,8 @@ export default function Home() {
               type="text"
               id="location"
               placeholder="Enter Location"
+              value={location}
+              onChange={(e) => setLocation(e.target.value)}
               className="w-full border rounded-md py-2 px-3 text-gray-700"
             />
           </div>
@@ -21,6 +51,8 @@ export default function Home() {
               <input
                 type="number"
                 id="minCustomers"
+                value={minCustomers}
+                onChange={(e) => setMinCustomers(e.target.value)}
                 className="w-full border rounded-md py-2 px-3 text-gray-700"
               />
             </div>
@@ -30,6 +62,8 @@ export default function Home() {
               <input
                 type="number"
                 id="maxCustomers"
+                value={maxCustomers}
+                onChange={(e) => setMaxCustomers(e.target.value)}
                 className="w-full border rounded-md py-2 px-3 text-gray-700"
               />
             </div>
@@ -40,13 +74,21 @@ export default function Home() {
                 type="number"
                 step="0.1"
                 id="avgCookies"
+                value={avgCookies}
+                onChange={(e) => setAvgCookies(e.target.value)}
                 className="w-full border rounded-md py-2 px-3 text-gray-700"
               />
             </div>
           </div>
 
           <div>
-            <button type="submit" className="bg-blue-500 text-white py-2 px-4 rounded-md">Create</button>
+            <button
+              type="button"
+              onClick={handleCreateClick}
+              className="bg-blue-500 text-white py-2 px-4 rounded-md"
+            >
+              Create
+            </button>
           </div>
         </form>
       </div>
@@ -56,6 +98,14 @@ export default function Home() {
   return (
     <div>
       <CookieStandForm />
+      {cookieStandData && (
+        <div className="mt-4 text-center">
+          <p className="font-semibold">Report Table Coming Soon...</p>
+          <pre className="inline-block p-2 border border-gray-300 rounded-md">
+            {`{"Location": "${cookieStandData.Location}", "Minimum Customers Per Hour": ${cookieStandData['Minimum Customers Per Hour']}, "Maximum Customers Per Hour": ${cookieStandData['Maximum Customers Per Hour']}, "Average Cookies Per Sale": ${cookieStandData['Average Cookies Per Sale']}}`}
+          </pre>
+        </div>
+      )}
     </div>
   );
 }
